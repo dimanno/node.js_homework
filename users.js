2ab32dc3953fd5994c80820b9223dba64106a790
dimanno <dimanno.dev@gmail.com>
2021-10-01 11:28:37 +0300

    sort users

--- users.js
+++ users.js
@@ -90,17 +90,38 @@ fs.readdir(path.join(__dirname, 'users'), ((err, files) => {
     }
     console.log(files)
     files.forEach(file => {
-        fs.readFile(path.join(__dirname, 'users', `${file}`), ((err,data)=>{
+        const filePath = path.join(__dirname, 'users', `${file}`);
+        fs.readFile(filePath, ((err,data)=>{
             if (err) {
                 console.log(err)
                 return;
             }
             let users = JSON.parse(data)
-            console.log(data);
-        }), )
+            console.log(users);
+            if ((users.age >  Number('20')) && (users.gender = 'male'))
+            {
+                fs.rename(filePath, path.join(__dirname, 'manOlder20', `new${file}`), ((err)=>{
+                    console.log(err);
+                }))
+            } else if ((users.age <= Number('20')) && (users.gender = 'male'))
+            {
+                fs.rename(filePath, path.join(__dirname, 'manYounger20', `new${file}`), ((err)=>{
+                    console.log(err);
+                }))
+            } else if ((users.age > Number('20')) && (users.gender = 'female')) {
+                fs.rename(filePath, path.join(__dirname, 'womanOlder20', `new${file}`), ((err) => {
+                    console.log(err);
+                }))
+            } else {
+                fs.rename(filePath, path.join(__dirname, 'womanYounger20', `new${file}`), ((err) => {
+                    console.log(err);
+                }))
+            }
+        }))
     })
     })
-)
+    )
+
 
 
 
