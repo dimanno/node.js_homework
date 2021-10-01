1eaf52566d126665b6aa3dd0bfad7b6e917c5985
dimanno <dimanno.dev@gmail.com>
2021-10-01 15:04:30 +0300

    clear master

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
-const fs = require('fs');
-const path = require('path')
-const users = require('./users')
-
-fs.mkdir(path.join(__dirname, 'manOlder20'), ((err)=>{
-    return console.log(err)
-}))
-
-fs.mkdir(path.join(__dirname, 'manYounger20'), ((err )=> {
-   return  console.log(err)
-}))
-
-fs.mkdir(path.join(__dirname, 'womanOlder20'), ((err )=> {
-    return console.log(err)
-}))
-
-fs.mkdir(path.join(__dirname, 'womanYounger20'), ((err) => {
-   return  console.log(err)
-}))
 
 
--- helper.js
+++
@@ -1,4 +0,0 @@
-const usersSort = require('./users');
-const fs = require('fs');
-
-usersSort
