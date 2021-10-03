c566c39c7013f5e0c9de4e85912292385156d8cb
dimanno <dimanno.dev@gmail.com>
2021-10-03 20:25:00 +0300

    start HW1.1

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathBoys = path.join(__dirname, 'Boys');
+const pathGirls = path.join(__dirname, 'Girls');
+
+const sortUsersByGenders = (gender, currentPath, newPath) => {
+    fs.readdir(pathBoys, (err, data)=> {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log(data)
+    })
+}
 
