0752654f8919d9099b703f3cc2e4bc69ab30c055
dimanno <dimanno.dev@gmail.com>
2021-10-03 22:47:41 +0300

    dane HW1.1

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
+const sort = require('./sortUsers');
 
 const pathBoys = path.join(__dirname, 'Boys');
 const pathGirls = path.join(__dirname, 'Girls');
 
-const sortUsersByGenders = (gender, currentPath, newPath) => {
-    fs.readdir(pathBoys, (err, data)=> {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log(data)
-    })
-}
+sort.sortUsersByGender('male', pathBoys, pathGirls);
+sort.sortUsersByGender('female', pathGirls, pathBoys);
 
---
+++ sortUsers.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const sortUsersByGender = (gender, currentPath, newPath) => {
+    fs.readdir(currentPath, (err, data)=> {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        data.forEach(files=> {
+            fs.readFile(path.join(currentPath, `${files}`), (err,data)=>{
+                if (err) {
+                    console.log(err);
+                    return
+                }
+
+                const user =  JSON.parse(data.toString());
+                const currentPathOfFile = path.join(currentPath, `${files}`);
+                const newPathOfFile = path.join(newPath, `${files}`);
+
+                if (gender === user.gender) {
+                    fs.rename(currentPathOfFile, newPathOfFile, (err)=>{
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                }
+            });
+        });
+    });
+}
+
+module.exports = {
+    sortUsersByGender
+}
