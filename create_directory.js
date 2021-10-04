3ec7d2dcefa7e3fb5ee1e981c1f6aacc0ffeabe6
dimanno <dimanno.dev@gmail.com>
2021-10-04 14:26:09 +0300

    dane HW1.1

--- create_directory.js
+++ create_directory.js
@@ -5,22 +5,24 @@ fs.mkdir(path.join(__dirname, 'manOlder20'), ((err)=>{
     if (err) {
         console.log(err)
     }
+    fs.mkdir(path.join(__dirname, 'manYounger20'), ((err )=> {
+        if (err) {
+            console.log(err)
+        }
+        fs.mkdir(path.join(__dirname, 'womanOlder20'), ((err )=> {
+            if (err) {
+                console.log(err)
+            }
+            fs.mkdir(path.join(__dirname, 'womanYounger20'), ((err) => {
+                if (err) {
+                    console.log(err)
+                }
+            }));
+        }));
+    }));
 }));
 
-fs.mkdir(path.join(__dirname, 'manYounger20'), ((err )=> {
-    if (err) {
-        console.log(err)
-    }
-}));
 
-fs.mkdir(path.join(__dirname, 'womanOlder20'), ((err )=> {
-    if (err) {
-        console.log(err)
-    }
-}));
 
-fs.mkdir(path.join(__dirname, 'womanYounger20'), ((err) => {
-    if (err) {
-        console.log(err)
-    }
-}));
+
+
