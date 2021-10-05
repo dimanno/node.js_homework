61b8fc28f64c7a4a4e45bad7e3dd9725f5cca88d
dimanno <dimanno.dev@gmail.com>
2021-10-05 12:30:32 +0300

    start work

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    console.log(req)
+    res.json("Hello Node JS")
+})
+
+app.listen(6000, () => {
+    console.log('app.listen 5000')
+})
 
 
