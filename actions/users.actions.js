cc01d981a90ac82acb330e4c68efc0a603b89979
dimanno <dimanno.dev@gmail.com>
2021-10-07 14:31:36 +0300

    hw3 test

--- actions/users.actions.js
+++ actions/users.actions.js
@@ -3,29 +3,29 @@ const path = require('path');
 
 const usersPath = path.join(__dirname, '..', 'database', 'users.json');
 
-const readF = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(usersPath, (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            let usersJSON = data.toString();
-            const users = JSON.parse(usersJSON);
-            resolve(users);
-        });
-    });
-};
+const readF = () => new Promise((resolve, reject) => {
+    fs.readFile(usersPath, (err, data) => {
 
-const writeF = users => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(usersPath, JSON.stringify(users), (err) => {
-            if (err) {
-                reject(err);
-            }
-            resolve();
-        });
+        if (err) {
+            reject(err);
+        }
+
+        const usersJSON = data.toString();
+        const users = JSON.parse(usersJSON);
+        resolve(users);
     });
-};
+});
+
+const writeF = users => new Promise((resolve, reject) => {
+    fs.writeFile(usersPath, JSON.stringify(users), (err) => {
+
+        if (err) {
+            reject(err);
+        }
+
+        resolve();
+    });
+});
 
 module.exports = {
     readF,
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const userRouter = require('./routers/users.router');
-const user_idRouter = require('./routers/users_id.router');
+const user_idRouter = require('./routers/users_id.router')
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 app.use('/users', userRouter, user_idRouter);
 
 app.listen(5001, () => {
