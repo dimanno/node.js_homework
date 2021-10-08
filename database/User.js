2bd7a4b240a4ca478428898bf739b62c7207e07a
dimanno <dimanno.dev@gmail.com>
2021-10-09 01:50:28 +0300

    hw3 after correct

--- database/User.js
+++ database/User.js
@@ -21,7 +21,6 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        unique: true,
         required: true
     },
     role: {
--- routers/router.login.js
+++ routers/router.login.js
@@ -3,6 +3,6 @@ const router= require('express').Router();
 const userLoginMiddleware = require('../middleware/userLogin.middleware');
 const userController = require('../controllers/user.controller');
 
-router.post('/:login', userLoginMiddleware.loginUserMiddleware, userController.loginUser);
+router.post('/login', userLoginMiddleware.loginUserMiddleware, userController.loginUser);
 
 module.exports = router;
